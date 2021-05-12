23077775bb37c242b4fb84e2636f5be9768a7123
Anna <lap_anna_v@mail.ru>
2021-05-13 00:39:36 +0300

    instructions in ToDo

--- src/app/index.js
+++ src/app/index.js
@@ -5,12 +5,22 @@ $(document).ready(function() {
   let todoHeaderDone = $("#todo-done");
   let todoListDone = $("#todo-list-done");
   let todoListNotDo = $("#todo-list-not-do");
+  let btnOpenInstr = $("#how-do_btn");
+  let instruction = $("#how-do_instr");
 
   todoHeaderNotDo.on("click", function (event) {
     if (todoListNotDo.hasClass("hide")) {
       todoListNotDo.removeClass("hide");
     } else {
       todoListNotDo.addClass("hide");
+    }
+  })
+
+  btnOpenInstr.on("click", function (event) {
+    if (instruction.hasClass("hide")) {
+      instruction.removeClass("hide");
+    } else {
+      instruction.addClass("hide");
     }
   })
 
