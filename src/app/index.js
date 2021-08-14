ada911ca86056bd29dfc62ae11c25ed9dd907acd
Anna <lap_anna_v@mail.ru>
2021-08-14 03:33:32 +0300

    added video for landing, updated todo

--- src/app/index.js
+++ src/app/index.js
@@ -134,85 +134,86 @@ if ($(".todo-list_wrapper")) {
   let arrTask = $(".el_task");
 
   let arrBtnCheck = $(".el-task-checkbox");
-
-  let arrTaskNotDo = []
-  let arrBtnReturnToListTask = []
-
-  let arrTaskWillDo = []
-  let arrBtnSetNotDo = []
+  let arrRisksBtn = $(".risks-btn")
 
   let arrMarker = $(".marker")
+  let isTaskRefuse = false
+  let amountTaskRefused = $(".todo-not-do").find(".list-amount").text() - 0
+  let amountTaskReady = $(".todo-done").find(".list-amount").text() - 0
 
+  function refuseTask (i) {
+    arrRisksBtn.eq(i).on("click", function (e) {
+      const taskId = e.target.dataset.taskId;
+      var isTaskRefuse = $(e.target).data('isrefused')
+      var parent =$(e.target).parents()[4]
 
-  function getArrTaskNotDo () {
-    arrTaskNotDo = $(".todo-el_not-do");
-    arrBtnReturnToListTask = $(".todo-el_not-do").find(".risks-btn");
-    for (var i = 0; i < arrTaskNotDo.length; i++) {
-        returnToListTask(i)
-    }
-    console.log("arrTaskNotDo", arrTaskNotDo.length)
-    console.log("arrBtnReturnToListTask", arrBtnReturnToListTask.length)
-  }
-
-  function getArrTaskWillDo () {
-    arrTaskWillDo = $(".todo-el").not(".todo-el_not-do")
-    arrBtnSetNotDo = $(".todo-el").not(".todo-el_not-do").find(".risks-btn");
-    console.log("arrTaskWillDo", arrTaskWillDo)
-    console.log("arrBtnSetNotDo", arrBtnSetNotDo)
-    for (var i = 0; i < arrTaskWillDo.length; i++) {
-        addToListNotDo(i)
-    }
-
-    console.log("-----------")
-  }
-
-  getArrTaskNotDo ()
-  getArrTaskWillDo ()
-
-
-  function returnToListTask (i) { //вернуть задачу в список задач для выполнения
-    console.log("i--", i)
-    arrBtnReturnToListTask.eq(i).on("click", function (event) {
-      console.log("click_returnToListTask", arrBtnReturnToListTask.eq(i))
-      arrBtnReturnToListTask.eq(i).text("Не хочу делать")
-      arrTaskNotDo.eq(i).find(".risks-text").text("Не хочу это делать, с некоторыми рисками можно смириться.")
-      arrTaskNotDo.eq(i).find(".el_task").removeClass("el_task-not-do")
-      arrTaskNotDo.eq(i).removeClass("todo-el_not-do")
-
-      // getArrTaskNotDo ()
-      // getArrTaskWillDo ()
-    })
-  }
-
-
-  function addToListNotDo (i) { //добавить  задачу в список задач которые не хочется делать
-    console.log("i-", i)
-
-    arrBtnSetNotDo.eq(i).on("click", function (event) {
-
-      console.log("i", i)
-      console.log("click_addToListNotDo",arrTaskWillDo.eq(i))
-      arrBtnSetNotDo.eq(i).text("Вернуть в список моих задач")
-      arrTaskWillDo.eq(i).find(".risks-text").html("Вы отказались от этот задачи.<br>Ничего страшного.")
-      arrTaskWillDo.eq(i).find(".el_task").addClass("el_task-not-do")
-      arrTaskWillDo.eq(i).addClass("todo-el_not-do")
-      console.log("done")
-      getArrTaskNotDo ()
-      getArrTaskWillDo ()
-
-    })
-  }
-
-
-
-  function checkBtn (i) { //оправлять на сервер отмеченную таску
-      arrBtnCheck.eq(i).on("change", function (e) {
-        if (arrTask.eq(i).hasClass("el_task-not-do")) {
-          arrTask.eq(i).removeClass("el_task-not-do")
-        }
+      const csrfmiddlewaretoken = $("[name=csrfmiddlewaretoken]")[0].value
+      $.post('/tasks/'+taskId+'/toggle/', {
+        'csrfmiddlewaretoken': csrfmiddlewaretoken,
+        'refuse': true,
       })
-    }
 
+      if (isTaskRefuse) { // УДАЛЯЕТ задачу из списка не хочу делать
+        amountTaskRefused -= 1
+        isTaskRefuse = 0
+        $(e.target).text("Не хочу делать")
+        $(parent).find($(".risks-text")).text("Не хочу это делать, с некоторыми рисками можно смириться.")
+        $(parent).find($(".el_task")).removeClass("el_task-not-do")
+        $(parent).removeClass($("todo-el_not-do"))
+        showNotification("Задание добавлено в Туду-лист")
+        addTimerClosing()
+      } else { // ДОБАВЛЯЕТ задачу из списка не хочу делать
+        $(e.target).text("Вернуть в список моих задач")
+        $(parent).find($(".risks-text")).html("Вы отказались от этот задачи.<br>Ничего страшного.")
+        $(parent).find($(".el_task")).addClass("el_task-not-do")
+        $(parent).addClass($("todo-el_not-do"))
+        showNotification("Перемещено в список 'Не хочу делать'")
+        addTimerClosing()
+        amountTaskRefused += 1
+        isTaskRefuse = 1
+      }
+      $(e.target).data('isrefused', isTaskRefuse)
+      $(parent).data('isrefused', isTaskRefuse)
+      $(".todo-not-do").find(".list-amount").text(amountTaskRefused)
+
+      e.stopPropagation();
+    })
+  }
+  for (var i = 0; i < arrRisksBtn.length; i++) {
+    refuseTask(i)
+  }
+
+
+  function checkBtn (i) {
+    arrBtnCheck.eq(i).on("change", function (e) {
+      const taskId = e.target.dataset.taskId;
+      var isTaskReady = $(e.target).data('isready')
+      const csrfmiddlewaretoken = $("[name=csrfmiddlewaretoken]")[0].value
+      $.post('/tasks/'+taskId+'/toggle/', {
+        'csrfmiddlewaretoken': csrfmiddlewaretoken,
+        'check': true,
+      })
+      if (isTaskReady) {
+        amountTaskReady -= 1
+        isTaskReady = 0
+        showNotification("Задание добавлено в Туду-лист")
+        addTimerClosing()
+      } else {
+        amountTaskReady += 1
+        isTaskReady = 1
+        showNotification("Здорово!")
+        addTimerClosing()
+      }
+      $(".todo-done").find(".list-amount").text(amountTaskReady)
+      $(e.target).data('isready', isTaskReady)
+      $(e.target).find($('.risks-btn')).data('isready', isTaskReady)
+
+      if (arrTask.eq(i).hasClass("el_task-not-do")) {
+        arrTask.eq(i).removeClass("el_task-not-do")
+
+      }
+    })
+  }
   for (var i = 0; i < arrBtnCheck.length; i++) {
     checkBtn(i)
     arrMarker.eq(i).text(i+1)
