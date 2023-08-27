5eb852eba5b3f9d57cb762651bde8507c8e39f33
Anna <lap_anna_v@mail.ru>
2023-08-28 00:21:11 +0100

    added popup full course

--- src/public/team/teamChart.js
+++ src/public/team/teamChart.js
@@ -216,3 +216,88 @@ $(() => {
   }).dxChart('instance');
 });
 
+
+$(() => {
+  $('#memberChart').dxChart({
+    dataSource: dataSource,
+    commonSeriesSettings: {
+      argumentType: 'datetime',
+      aggregationInterval: 'day',
+      valueMarginsEnabled: true,
+      argumentField: 'date',
+      type: 'stackedBar',
+      point: {
+        hoverMode: 'allArgumentPoints',
+      },
+    },
+    series: [
+      { valueField: 'resolvedTask', name: 'Выполнено заданий', color:"#2E2E38", barWidth: '20'},
+      { valueField: 'completedTaskDigital', name: 'Цифровая безопасность', color:"#0EA767", barWidth: '20' },
+      { valueField: 'completedTaskPersonal', name: 'Личная безопасность',  color:"#605DF9", barWidth: '20' },
+      { valueField: 'completedTaskFinance', name: 'Финансовая безопасность', color: "#29C5F6", barWidth: '20'}
+    ],
+
+    argumentAxis: {
+      visualRange: {
+        length: 30
+      },
+
+      label: {
+        customizeText: (arg) => {return new Date(arg.value).getDate()}
+      },
+      tick: {
+        color:'transparent'
+      },
+      tickInterval: { days: 1 },
+      color:'transparent',
+    },
+
+    scrollBar: {
+      visible: true,
+    },
+    zoomAndPan: {
+      argumentAxis: 'both',
+    },
+    legend: {
+      visible: false,
+    },
+    valueAxis: {
+      tick: {
+        color:'transparent'
+      },
+      color: 'transparent',
+      grid: {
+        color:'#aeaeb21a'
+      },
+    },
+    panes: {
+      backgroundColor: "#F9F9FB",
+    },
+    tooltip: {
+      enabled: true,
+      location: 'edge',
+      border: {
+        color: '#F3F3F6',
+
+      },
+      cornerRadius: 10,
+      shadow: {
+        offsetX: 5,
+        color: 'rgba(0, 0, 0, 0.04)',
+        offsetY: 5,
+      },
+      arrowLength: 0,
+      contentTemplate(info, container) {
+        const contentItems = ["<div class='teamChart_tooltip'>",
+        `<div class='teamChart_tooltip_item teamChart_tooltip_title tooltip_resolvedTask'><span class='tooltip_item-title'>Выполнено заданий</span><span class='tooltip_item-value'>${info.point.data.resolvedTask ? info.point.data.resolvedTask : '—'}</span></div>`,
+          "<div class='teamChart_tooltip_title tooltip_completedTask'><span class='tooltip_item-title'>Пройдено занятий</span>",
+          `<div class='teamChart_tooltip_item teamChart_tooltip_name tooltip_completedTask-digital'><span class=''>Цифровая безопасность</span><span class='tooltip_item-value'>${info.point.data.completedTaskDigital ? info.point.data.completedTaskDigital : '—'}</span></div>`,
+          `<div class='teamChart_tooltip_item teamChart_tooltip_name tooltip_completedTask-personal'><span class=''>Личная безопасность</span><span class='tooltip_item-value'>${info.point.data.completedTaskPersonal ? info.point.data.completedTaskPersonal : '—'}</span></div>`,
+          `<div class='teamChart_tooltip_item teamChart_tooltip_name tooltip_completedTask-finance'><span class=''>Финансовая безопасность</span><span class='tooltip_item-value'>${info.point.data.completedTaskFinance ? info.point.data.completedTaskFinance : '—'}</span></div>`,
+       '</div></div>'];
+        const content = $(contentItems.join(''));
+        content.appendTo(container);
+  },
+    },
+  }).dxChart('instance');
+});
